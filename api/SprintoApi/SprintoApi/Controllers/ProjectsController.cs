d02b64b291dfce8d3dfeea9530149393471502f1
Nizar Sweiss <92578643+Nizar-Sweiss@users.noreply.github.com>
2025-07-05 17:43:59 +0300

    [API] : projects controller added along with fetching and adding project functions

---
+++ api/SprintoApi/SprintoApi/Controllers/ProjectsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SprintoApi.Data;
+using SprintoApi.Models;
+
+namespace SprintoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly SprintoDbContext dbContext;
+        public ProjectsController(SprintoDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpPost]
+        [Route("getUserProjects")]
+        public IActionResult getUserProjects(ProjectsDto projectsDto)
+        {
+            List<Projects> projects = dbContext.projects.ToList().Where(e => e.created_by == projectsDto.created_by).ToList();
+
+            return Ok(projects);
+
+        }
+
+        [HttpPost]
+        [Route("addProject")]
+        public IActionResult adaddProjectdUser(Projects project)
+        {
+            Projects newproject = new Projects { created_by = project.created_by, created_date = DateTime.Now, description = project.description, title = project.title };
+            dbContext.projects.Add(newproject);
+            dbContext.SaveChanges();
+
+            return Ok(newproject);
+
+        }
+    }
+}
---
+++ api/SprintoApi/SprintoApi/Migrations/20250705065908_Tasks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SprintoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tasks",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    project_id = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false),
+                    due_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    created_by = table.Column<int>(type: "int", nullable: false),
+                    created_date = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tasks", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tasks");
+        }
+    }
+}
