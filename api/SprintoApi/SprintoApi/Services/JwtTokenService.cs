d31e917137da1c31ad9fd7536129b0c594bee4be
Nizar Sweiss <92578643+Nizar-Sweiss@users.noreply.github.com>
2025-07-09 21:16:15 +0300

    [Api] add JWT to the login function

---
+++ api/SprintoApi/SprintoApi/Services/JwtTokenService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SprintoApi.Services
+{
+    public class JwtTokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+     
+        public string GenerateToken(string username, int userId)
+        {
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username),   
+                new Claim("userId", userId.ToString())        
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- api/SprintoApi/SprintoApi/obj/Debug/net8.0/SprintoApi.AssemblyInfo.cs
+++ api/SprintoApi/SprintoApi/obj/Debug/net8.0/SprintoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SprintoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485145bf148df22418d5e9eeedf1d2b475b17d04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ed7cbb01b185ff377e3fe6225adda03ea35068")]
 [assembly: System.Reflection.AssemblyProductAttribute("SprintoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SprintoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
